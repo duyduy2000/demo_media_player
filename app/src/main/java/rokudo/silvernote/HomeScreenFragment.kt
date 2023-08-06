ed723a352de17b8ef1a8fac973a22a5c25ff8689
duyduy2000 <124852416+dark-blue-02@users.noreply.github.com>
2023-08-06 22:37:33 +0700

    Initial project

---
+++ app/src/main/java/rokudo/silvernote/HomeScreenFragment.kt
@@ -0,0 +1,29 @@
+package rokudo.silvernote
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import rokudo.silvernote.databinding.FragmentHomeScreenBinding
+
+class HomeScreenFragment : Fragment() {
+
+    private var _binding: FragmentHomeScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
